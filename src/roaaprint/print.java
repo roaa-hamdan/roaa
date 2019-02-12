07a4f3b75e562f878b627c926e4282626c668e01
roaahamdan <roaahamdan@192.168.1.7>
2019-02-12 17:50:32 +0200

    roaaprint

---
+++ src/roaaprint/print.java
@@ -0,0 +1,13 @@
+package roaaprint;
+
+import javax.swing.JOptionPane;
+
+public class print {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null, "some required fields are empty ,please fill them ");
+
+	}
+
+}
